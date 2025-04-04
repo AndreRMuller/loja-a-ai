dd8e27a0b23132234427f6776b56408ddb32fb01
AndreRMuller <andrerafaelmuller123@gmail.com>
2025-04-04 19:30:20 -0300

    terceiro

--- back/modal/Usuario.ts
+++ back/modal/Usuario.ts
@@ -8,8 +8,8 @@ export class Usuario {
     senha: string;
 
     public async insert():Promise<Usuario|null>{
-        let sql = `INSERT INTO "usuarios" ("id" , "nome" , "email" , "senha")
-         VALUES("$1",$2,$3,$4) RETURNING id`;
+        let sql = `INSERT INTO "usuario" ("id" , "nome" , "email" , "senha")
+         VALUES("$1",$2,$3,$4) RETURNING id;`;
 
         let params = [
             this.id,
@@ -30,5 +30,65 @@ export class Usuario {
          return null;
 
     }
+
+    public async Update():Promise<Usuario|null>{
+
+        let sql = `UPDATE "usuario" SET "nome" = $1, "senha" = $2 WHERE "id" = $3 ;`
+
+        let params = [
+            this.nome,
+            this.senha,
+            this.id
+        ];
+
+        let result = await dbQuery(sql, params);
+
+        if(result) {
+            return this;
+        }
+
+        return null;
+    }
+
+    public async save():Promise<Usuario|null>{
+
+        if(this.id){
+            return await this.Update();
+        }
+
+        return await this.insert() ;
+    }
+
+
+    public async Delete():Promise<Usuario|null>{
+
+        let sql = `DELETE FROM "usuario" WHERE "id" = $1 RETURNING id;`
+
+        let resultado = await dbQuery(sql,[this.id]);
+
+        if(resultado.length > 0) {
+            this.id = resultado[0].id;
+            return this;
+        }
+        return null;
+    }
+
+    static async findOneById(id:number):Promise<Usuario|null>{
+
+        let sql = `SELECT * FROM "usuario" WHERE "id" = $1 LIMIT 1;`
+
+        let result = await dbQuery(sql,[id]);
+
+        if(result.length > 0) {
+
+            let usuario = new Usuario();
+            Object.assign(usuario, result[0]);
+            usuario.id = result[0].id;
+            return usuario;
+        }
+        return null;
+    }
+
+    
 
 };
